bba19c8638442962a024910107f0fdfdb4b977c7
jusplo <ploonka@gmail.com>
2017-03-14 19:35:29 +0100

    update2

---
+++ CalculatorGitHub/src/MyCalculator.java
@@ -0,0 +1,33 @@
+public class mycalculator implements Calculator {
+	
+	
+	public static void main(String[] args) {
+		
+			
+	}
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public int multiply(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	@Override
+	public double divide(double a, double b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
