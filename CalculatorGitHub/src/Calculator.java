2bed1bfeef715612b9bd9cc7750e329199df8062
grzegorzwidelka <grzegorz.widelka@gmail.com>
2017-03-14 19:46:28 +0100

    Ania musimy to skompilowac! Odbior!

--- CalculatorGitHub/src/Calculator.java
+++ CalculatorGitHub/src/Calculator.java
@@ -1,4 +1,9 @@
 
-public class Calculator {
+public interface Calculator {
+	
+	int add(int a, int b);
+	int substract(int a, int b);
+	int multiply(int a, int b);
+	double divide(double a, double b);
 
 }
---
+++ CalculatorGitHub/src/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Wybierz opcje: ");
+		System.out.println("1)+");
+		System.out.println("2)-");
+		System.out.println("3)*");
+		System.out.println("4)/");
+		String option =scanner.nextLine();
+		
+		num1 = nextInt(System.in);
+		
+		
+	}
+
+}
